ea8f98ea13d58ded4f6f47011cc86c13333ef8d3
AqibMirza91 <K1740497@kingston.ac.uk>
2020-06-30 10:40:05 +0100

    Progressing through course

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,14 +1,33 @@
-import React from 'react';
-import newTodoForm from './NewTodoForm';
-import TodoListItem from './TodoListItem';
-import './TodoList.css';
-import NewTodoForm from './NewTodoForm';
+import React from "react";
+import { connect } from "react-redux";
+import newTodoForm from "./NewTodoForm";
+import TodoListItem from "./TodoListItem";
+import { removoTodo, markTodoAsCompleted } from "./actions";
+import { displayAlert } from "./thunks";
+import "./TodoList.css";
+import NewTodoForm from "./NewTodoForm";
 
-const TodoList = ({ todos = [{ text: 'Hello' }] }) => (
-    <div className="list-wrapper">
-        <NewTodoForm />
-        {todos.map(todo => <TodoListItem todo={todo} />)}
-    </div>
+const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed, onDisplayAlert }) => (
+  <div className="list-wrapper">
+    <NewTodoForm />
+    {todos.map((todo) => (
+      <TodoListItem
+        todo={todo}
+        onRemovePressed={onRemovePressed}
+        onCompletedPressed={onCompletedPressed}
+      />
+    ))}
+  </div>
 );
 
-export default TodoList;
+const mapStatetoProps = (state) => ({
+  todos: state.todos,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onRemovePressed: (text) => dispatch(removoTodo(text)),
+  onCompletedPressed: (text) => dispatch(markTodoAsCompleted(text)),
+  onDisplayAlertClicked: () => dispatch(displayAlert),
+});
+
+export default connect(mapStatetoProps, mapDispatchToProps)(TodoList);
---
+++ src/todos/thunks.js
@@ -0,0 +1,8 @@
+// export const loadTodos = () = async (dispatch, getState) => {
+
+// };
+
+export const displayAlert = text => () => {
+    alert(`You clicked on: ${text}`);
+};
+
